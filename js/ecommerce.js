888c8a4a2ce0970fdaa71a76f82ca9a3d788b69e
Romuald01 <romualdadjimagbe2018@gmail.com>
2023-12-24 16:45:27 +0100

    updatinig fx ecommerce

--- js/ecommerce.js
+++ js/ecommerce.js
@@ -8,9 +8,6 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
-
-// This is script file
-
 $(".testimonials-container").owlCarousel({
   loop: true,
   autoplay: true,
@@ -35,3 +32,50 @@ $(".testimonials-container").owlCarousel({
     },
   },
 });
+
+var slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+// Auto Slide   if you need auto slide ,remove the commit "//"
+// var slideIndex = 0;
+// showSlides();
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {
+//     slideIndex = 1;
+//   }
+//   slides[slideIndex - 1].display = "block";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
+// about_us owl carousel
+// This is script file
